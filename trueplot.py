50cd4f6fbcd9b625312e332d61d4ed919cf92262
Trippwire <58035903+Trippwire2000@users.noreply.github.com>
2020-05-16 00:44:17 +1000

    Add files via upload
    basic gui, neater concise code.

--- trueplot.py
+++ trueplot.py
@@ -4,124 +4,67 @@ import pandas as pd
 import tkinter as tk
 
 root = tk.Tk()
-root.title = 'Plot Boy'
+root.title('Trueplot')
+root.geometry('400x400')
+
+#                    DICT NAME          [0]              [1]            [2]         [3]          [4]        [5]
+#             'text for drop menus: text for title, variable name, filename.csv, column data, date format, color '
+ds = {'Gold AUD': ['Gold AUD', 'GOLD_AUD', 'dsets/GOLD.csv', 'PRICE_AUD', '%d/%m/%y', 'm'],\
+			'Gold USD' : ['Gold USD', 'GOLD_USD', 'dsets/GOLD.csv', 'PRICE_USD', '%d/%m/%y', 'k'],\
+			'Silver AUD': ['Silver AUD', 'SILVER_AUD', 'dsets/SILVER.csv', 'PRICE_AUD', '%d/%m/%y', 'b'],\
+			'Silver USD': ['Silver USD', 'SILVER_USD', 'dsets/SILVER.csv', 'PRICE_USD', '%d/%m/%y', 'b'],\
+			'Platinum AUD': ['Platinum AUD', 'PLATINUM_AUD', 'dsets/PLATINUM.csv', 'PRICE_AUD', '%d/%m/%y', 'g'],\
+			'Platinum USD': ['Platinum USD', 'PLATINUM_USD', 'dsets/PLATINUM.csv', 'PRICE_USD', '%d/%m/%y', 'y'],\
+			'Oil, Brent USD': ['Oil, Brent USD', 'OIL_BRENT', 'dsets/OIL.csv', 'BRENT', '%Y-%m-%d', 'r'],\
+			'Oil, WTI USD': ['Oil, WTI USD', 'OIL_WTI', 'dsets/OIL.csv', 'WTI', '%Y-%m-%d', 'r'],\
+			'S&P 500': ['S&P 500', 'SP500', 'dsets/SP500.csv', 'SP500', '%Y-%m-%d', 'm'],\
+			'AUD vs USD': ['AUD vs USD', 'AUDvsUSD', 'dsets/AUDUSD.csv', 'PRICE', '%b %d, %Y', 'k'],\
+			'AUS Interest Rate': ['AUS Interest Rate', 'AU_INTEREST', 'dsets/AU_INTEREST.csv', 'RATE', '%Y-%m-%d', 'g']}
+
+dslist = list(ds.keys())															#SET DROP LIST FROM DICT(ds) KEYS
 
 def go():
+	y1data = y1button.get()
+	y2data = y2button.get()
 
-	fig, ax1 = plt.subplots()
+	fig, ax1 = plt.subplots()														#SET AS DUAL Y AXIS, SHARING COMMON X AXIS
 	ax2 = ax1.twinx()
 
-	axislist = [ax1, ax2]
-	getter = [y1.get(), y2.get()]
+	for axis, ydata in zip([ax1, ax2], [y1data, y2data]):
+		print(axis, ydata)
+		ds[ydata][1] = read_csv(ds[ydata][2], usecols = ['DATE', ds[ydata][3]])
+		ds[ydata][1] = ds[ydata][1].fillna(method = 'ffill')
+		ds[ydata][1]['DATE'] = pd.to_datetime(ds[ydata][1].DATE, format = ds[ydata][4])
+		axis.set_ylabel(ds[ydata][0])
+		axis.plot(ds[ydata][1]['DATE'], ds[ydata][1][ds[ydata][3]], label = ds[ydata][0], color = ds[ydata][5])
 
-	for i, y in zip(axislist, getter):
-
-		print(str(i), str(y))
-		if y == 'Gold AUD':
-			i.plot(gold['DATE'], gold['Gold AUD'], label = 'Gold AUD', color = 'm')
-			continue
-
-		elif y == 'Gold USD':
-			i.plot(gold['DATE'], gold['Gold USD'], label = 'Gold USD', color = 'm')
-			continue
-
-		elif y == 'Silver AUD':
-			i.plot(silver['DATE'], silver['Silver AUD'], label = 'Silver AUD', color = 'g')
-			continue
-
-		elif y == 'Silver USD':
-			i.plot(silver['DATE'], silver['Silver USD'], label = 'Silver USD', color = 'g')
-			continue
-
-		elif y == 'Platinum AUD':
-			i.plot(platinum['DATE'], platinum['Platinum AUD'], label = 'Platinum AUD', color = 'y')
-			continue
-
-		elif y == 'Platinum USD':
-			i.plot(platinum['DATE'], platinum['Platinum USD'], label = 'Platinum USD', color = 'y')
-			continue
-
-		elif y == 'Oil, Brent USD':
-			i.plot(oil['DATE'], oil['Oil Brent USD'], label = 'Brent Crude Oil', color = 'k')
-			continue
-
-		elif y == 'Oil, WTI USD':
-			i.plot(oil['DATE'], oil['Oil WTI USD'], label = 'WTI', color = 'r')
-			continue
-
-		elif y == 'Unemployment % USA':
-			i.plot(unemployed['DATE'], unemployed['Unemployment % USA'], label = 'USA unemployment %', color = 'b')
-			continue
-
-		elif y == 'SP500 USD':
-			i.plot(sp500['DATE'], sp500['SP500 USD'], label = 'SP500', color = 'b')
-
-
-
-	#plt.legend()
-	ax1.legend(loc = 0)
-	ax2.legend(loc = 1)
+	ax1.legend(loc = 1)																#SET LEGENDS TO TOP L & R CORNER
+	ax2.legend(loc = 2)
+	plt.title(ds[y1data][0] + ' / ' + ds[y2data][0])								#SET TITLE USING DICTIONARY
 	fig.tight_layout()
+
+	mng = plt.get_current_fig_manager()												#SET FIGURE TO FULLSCREEN
+	mng.full_screen_toggle()
 	plt.show()
 
 
-dslist = ['Gold AUD', 'Gold USD', 'Silver AUD', 'Silver USD', 'Platinum AUD', 'Platinum USD', 'SP500 USD', 'Oil, Brent USD', 'Oil, WTI USD', 'Unemployment % USA' ]
+
+y1button = tk.StringVar(root)														#SET PLOT 1 FROM LIST
+y1button.set(dslist[0])
+y1buttondrop = tk.OptionMenu(root, y1button, *dslist)
+y1buttondrop.pack(expand = True, fill = 'both')
+
+y2button = tk.StringVar(root)														#SET PLOT 2 FROM LIST
+y2button.set(dslist[2])
+y2buttondrop = tk.OptionMenu(root, y2button, *dslist)
+y2buttondrop.pack(expand = True, fill = 'both')
 
 
-y1 = tk.StringVar(root)
-y1.set(dslist[0])
-y1drop = tk.OptionMenu(root, y1, *dslist)
-y1drop.pack(expand = True, fill = 'both')
-
-y2 = tk.StringVar(root)
-y2.set(dslist[2])
-y2drop = tk.OptionMenu(root, y2, *dslist)
-y2drop.pack(expand = True, fill = 'both')
-
-
-go_button = tk.Button(root, text = 'Plot chart', command = go)
+go_button = tk.Button(root, text = 'Plot chart', command = go)						#PLOT CHART FUNCTION
 go_button.pack(expand = True, fill = 'both')
 
-gold = read_csv('gold.csv', usecols = ['DATE', 'Gold AUD', 'Gold USD'])
-silver = read_csv('silver.csv', usecols = ['DATE', 'Silver USD', 'Silver AUD'])
-platinum = read_csv('platinum.csv', usecols = ['DATE', 'Platinum AUD', 'Platinum USD'])
-sp500 = read_csv('sp-500.csv', usecols = ['DATE', 'SP500 USD'])
-oil = read_csv('oil.csv', usecols = ['DATE', 'Oil Brent USD', 'Oil WTI USD'])
-unemployed = read_csv('usa_unemployment.csv', usecols = ['DATE', 'Unemployment % USA'])
 
-
-gold_raw = gold.fillna(method = 'ffill')
-sp500 = sp500.fillna(method = 'ffill')
-oil = oil.fillna(method = 'ffill')
-silver = silver.fillna(method = 'ffill')
-platinum = platinum.fillna(method = 'ffill')
-#silver_raw = silver_raw.fillna(axis = 0, how = 'any')
-#platinum_raw = platinum_raw.fillna(axis = 0, how = 'any')
-
-
-
-unemployed['DATE'] = pd.to_datetime(unemployed.DATE, format = '%Y')
-gold['DATE'] = pd.to_datetime(gold.DATE, format = '%d/%m/%y')
-sp500['DATE'] = pd.to_datetime(sp500.DATE, format = '%Y-%m-%d')
-oil['DATE'] = pd.to_datetime(oil.DATE, format = '%Y-%m-%d')
-silver['DATE'] = pd.to_datetime(silver.DATE, format = '%d/%m/%y')
-platinum['DATE'] = pd.to_datetime(platinum.DATE, format = '%d/%m/%y')
 
 
 
 root.mainloop()
-
-
-
-
-
-
-
-# ax2.plot(oil['DATE'], oil['Oil Brent USD'], label = 'Brent Crude Oil', color = 'k')
-# ax1.plot(oil['DATE'], oil['Oil WTI USD'], label = 'WTI', color = 'r')
-# ax1.plot(unemployed['DATE'], unemployed['Unemployment % USA'], label = 'USA unemployment %')
-# ax2.plot(silver['DATE'], silver['Silver USD'], label = 'Silver USD', color = 'g')
-# ax1.plot(platinum['DATE'], platinum['Platinum USD'], label = 'Platinum USD', color = 'y')
-# plt.plot(gold['DATE'], gold['Gold AUD'], label = 'Gold AUD')
-# ax1.plot(gold['DATE'], gold['Gold USD'], label = 'Gold USD')
-# ax2.plot(sp500['DATE'], sp500['SP500 USD'], label = 'SP500')
